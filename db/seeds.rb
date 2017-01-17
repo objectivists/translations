27d03fba161be5a4287d1766054f1e7b5e61f887
Dominic <db11289@gmail.com>
2017-01-16 19:31:58 -0800

    Adjusting based on feedback from PR:
    * Updated to display image rather than raw url (added seed data to help test)
    * Better BookController testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'Atlas Shrugged',
+            author: 'Ayn Rand',
+            cover_image_url: 'atlas_shrugged.jpg',
+            slug:'atlas-shrugged')
+
+Book.create(title: 'The Fountainhead',
+            author: 'Ayn Rand',
+            cover_image_url: 'fountainhead.jpg',
+            slug:'fountainhead')
+
+Book.create(title: 'Objectivism: The Philosophy of Ayn Rand',
+            author: 'Leonard Peikoff',
+            cover_image_url: 'opar.jpg',
+            slug:'objectivism-the-philosophy-of-ayn-rand')
